4733df4304755adf567801871333c229da6fee73
Std <student@Lab-1302.(none)>
2013-02-05 15:45:42 +0700

    edit,update, and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	
 	def index
-		
+		@users = User.all
 	end
 
 	def new
@@ -23,11 +23,20 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-
+		@user = User.find(params[:id])
 	end
 
 	def update
-
+		@user = User.find(params[:id])
+		respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def destroy
